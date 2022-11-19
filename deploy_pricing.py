9fbe29bd6e4ae3f0a0ab22bf173c0317e0f24bca
Thomas Marchand <thomas.marchand@tuta.io>
2022-11-19 11:51:17 +0100

    simplify pricing deployment

--- deploy_pricing.py
+++ deploy_pricing.py
@@ -11,12 +11,7 @@ import sys
 
 argv = sys.argv
 
-deployer_account_addr = (
-    0x048F24D0D0618FA31813DB91A45D8BE6C50749E5E19EC699092CE29ABE809294
-)
-deployer_account_private_key = int(argv[1])
-token = argv[2] if len(argv) > 2 else None
-admin = 0x048F24D0D0618FA31813DB91A45D8BE6C50749E5E19EC699092CE29ABE809294
+token = argv[1] if len(argv) > 1 else None
 # MAINNET: https://alpha-mainnet.starknet.io
 # TESTNET: https://alpha4.starknet.io
 # TESTNET2: https://alpha4-2.starknet.io
@@ -33,13 +28,6 @@ async def main():
             "feeder_gateway_url": network_base_url + "feeder_gateway",
             "gateway_url": network_base_url + "gateway",
         }
-    )
-    account = AccountClient(
-        client=client,
-        address=deployer_account_addr,
-        key_pair=KeyPair.from_private_key(deployer_account_private_key),
-        chain=chainid,
-        supported_tx_version=1,
     )
 
     pricing_file = open("./build/pricing.json", "r")
